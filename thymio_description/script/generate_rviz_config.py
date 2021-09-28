a3f9acb6161a7e3e9883b2669a541dfd67494cd7
Jerome Guzzi <jerome@idsia.ch>
2021-09-28 10:42:13 +0200

    Correct substitution

--- thymio_description/script/generate_rviz_config.py
+++ thymio_description/script/generate_rviz_config.py
@@ -8,11 +8,11 @@ def rviz(launch_folder: str, name: str) -> None:
     source_path = os.path.join(launch_folder, 'template.rviz')
     with open(source_path, 'r') as f:
         source = f.read()
-    if name and name[0] != "/":
-        ns = f"/{name}"
+    if not name:
+        dest = source.replace('/thymio', '')
     else:
-        ns = name
-    dest = source.replace('/thymio', ns).replace('thymio', name)
+        dest = source
+    dest = dest.replace('thymio', name)
     dest_path = os.path.join(launch_folder, 'thymio.rviz')
     with open(dest_path, 'w') as f:
         f.write(dest)
